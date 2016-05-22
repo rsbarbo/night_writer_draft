990f0be87fd98d8d0d54d337df49f9bba6e93ff9
Raphael Barbo <rsbarbo@gmail.com>
2016-05-22 17:15:07 -0600

    latest update sunday

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -15,12 +15,12 @@ class NightWriter
     braille = encode_to_braille(plain)
   end
 
+  #This method will call all the additional methods.
   def encode_to_braille(input)
     braille_array = check_input_vs_alphabet(input.split(''))
   end
 
-  #convert a string into an array|iterate each element|check each element|
-  #build new array with braille conversion
+  #cnvrt strng into array|itrte each elemnt|chck each elemnt|build new array w/ brl cnvrt
   def check_input_vs_alphabet(string_array)
     braille_translated = []
     string_array.each do |letter|
--- test/night_reader_test.rb
+++ test/night_reader_test.rb
@@ -6,8 +6,5 @@ require_relative "../lib/alphabet_key"
 
 class ReaderTest < Minitest::Test
 
-def test_translate_to_braille
-    translator = Alphabet.new.key
-end
 
 end
