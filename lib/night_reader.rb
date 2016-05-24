926f4763afce28b1fea64d15550a9d7679b340a5
Raphael Barbo <rsbarbo@gmail.com>
2016-05-24 15:55:44 -0600

    latest update

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -5,47 +5,35 @@ require './lib/file_reader'
 class NightReader
   attr_reader :reader, :alpha
 
-  def initialize
-    @reader = FileReader.new
-    @alpha = Alphabet.new
-  end
-
-  def encode_file_to_braille
-    plain = reader.read
-    braille = encode_to_braille(plain)
-  end
-
-  #This method will call all the additional methods.
-  def encode_to_english(input)
-    braille_array = check_input_vs_alphabet(input.split(''))
-
-  end
-
-  #cnvrt strng into array|itrte each elemnt|chck each elemnt|build new array w/ brl cnvrt
-  def check_input_vs_alphabet(string_array)
-    braille_translated = []
-    string_array.each do |letter|
-      if letter == letter.upcase && letter != letter.downcase
-        braille_translated << alpha.trans_braille[:shift]
-        braille_translated << alpha.trans_braille[letter.downcase]
-      else
-        braille_translated << alpha.trans_braille[letter]
-      end
-    end
-    sort_out(braille_translated)
-  end
-
-  def sort_out(braille_translated)
-  line_1 = []
-  line_2 = []
-  line_3 = []
-
-  braille_translated.each do |letter|
-    line_1 << letter[0]
-    line_2 << letter[1]
-    line_3 << letter[2]
-  end
-  final_brl_transl = "#{line_1.join}\n#{line_2.join}\n#{line_3.join}"
-  end
+  def transprep_brl_eng
+ # =begin
+ #
+ #     STEP #1
+ #
+ #     grab all the elements on line_1, line_2, line_3, and shove in into a new empty
+ #     array, example
+ #     (line_1 []) << ..0.0.0.0.0......00.0.0.00..
+ #     (line_2 []) << ..00.00.0..0....00.0000..000
+ #     (line_3 []) << .0....0.0.0....0.00.0.0...0.
+ #
+ #     After input has been shoved into an array, need to split it into strings
+ #     example: ["..","0.","0.","0.","0.","0.","..","..",".0","0.","0.","0.","00",".."]
+ #
+ #     STEP #2
+ #
+ #     Create an empty array with a variable name "prep_to_trans" = [])
+ #
+ #     prep_to_trans = []
+ #     prep_to_trans << [[line_1], [line_2], [line_3]]
+ #
+ #     STEP #3
+ #
+ #     sort_out(english_translated)
+ #
+ #     need to sort out line_1[0], line_2[0], line_3[0] into an empty array
+ #     and so on for all the remainder strings(letters)
+ #
+ # =end
+ #
 
 end
